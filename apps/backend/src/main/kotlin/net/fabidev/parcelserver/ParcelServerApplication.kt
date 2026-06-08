9811a3e0a615a7d622b102f1e40d7fa12ecc2970
Fabio Gaming <fabio.furch@gmail.com>
2026-06-08 03:11:22 +0200

    excluded redis from being autoloaded for now

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/ParcelServerApplication.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/ParcelServerApplication.kt
@@ -1,11 +1,15 @@
 package net.fabidev.parcelserver
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataRedisAutoConfiguration::class])
 class ParcelServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ParcelServerApplication>(*args)
 }
+
