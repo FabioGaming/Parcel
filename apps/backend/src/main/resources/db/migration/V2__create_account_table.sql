2bc005d3f3c8944e26c0fcc53567f913f3af0dd7
Fabio Gaming <fabio.furch@gmail.com>
2026-06-08 08:35:46 +0200

    added password field to auth.account table

--- apps/backend/src/main/resources/db/migration/V2__create_account_table.sql
+++ apps/backend/src/main/resources/db/migration/V2__create_account_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS auth.account (
   id UUID NOT NULL PRIMARY KEY,
   email TEXT NOT NULL,
+  password_hash TEXT NOT NULL,
   profile_id UUID NULL,
   created_at TIMESTAMP NOT NULL,
   modified_at TIMESTAMP NOT NULL,
