d02759506270535fe3ffa82374bf34ba8541fe4c
Fabio Gaming <fabio.furch@gmail.com>
2026-06-12 15:58:08 +0200

    removed redundant imports from main class

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/ParcelServerApplication.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/ParcelServerApplication.kt
@@ -1,10 +1,8 @@
 package net.fabidev.parcelserver
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication(exclude = [DataRedisAutoConfiguration::class])
 class ParcelServerApplication
