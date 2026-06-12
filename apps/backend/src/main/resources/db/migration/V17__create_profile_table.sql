8a3f444ec11c237d81828824d7c3c271d0dc1cef
Fabio Gaming <fabio.furch@gmail.com>
2026-06-12 15:17:19 +0200

    modified profile.profile to have a unique avatar file id

--- apps/backend/src/main/resources/db/migration/V17__create_profile_table.sql
+++ apps/backend/src/main/resources/db/migration/V17__create_profile_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS profile.profile (
     id UUID NOT NULL PRIMARY KEY,
     display_name TEXT NOT NULL,
     avatar_url TEXT,
-    avatar_file_id UUID,
+    avatar_file_id UUID UNIQUE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
