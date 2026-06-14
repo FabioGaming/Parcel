a2893720ca22c3c923940cc6b711e83be53f2c37
Fabio Gaming <fabio.furch@gmail.com>
2026-06-14 03:07:33 +0200

    removed redis exclusion from main class loader

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/ParcelServerApplication.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/ParcelServerApplication.kt
@@ -4,10 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = [DataRedisAutoConfiguration::class])
+@SpringBootApplication
 class ParcelServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ParcelServerApplication>(*args)
 }
-
