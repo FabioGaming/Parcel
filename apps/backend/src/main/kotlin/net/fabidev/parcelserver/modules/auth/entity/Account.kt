1ddc5e648b6755623837d8b5241918023c286565
Fabio Gaming <fabio.furch@gmail.com>
2026-06-15 02:30:00 +0200

    Added Account.kt for auth.account table

---
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/entity/Account.kt
@@ -0,0 +1,41 @@
+package net.fabidev.parcelserver.modules.auth.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import net.fabidev.parcelserver.shared.AccountRole
+import java.time.Instant
+import java.util.UUID
+
+@Entity
+@Table(name = "account", schema = "auth" )
+class Account(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", unique = true, nullable = false)
+    var id: UUID? = null,
+
+    @Column(name = "email", unique = true, nullable = false)
+    var email: String,
+
+    @Column(name = "password_hash", nullable = false)
+    var passwordHash: String,
+
+    @Column(name = "profile_id", nullable = true)
+    var profileId: UUID? = null,
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: Instant,
+
+    @Column(name = "modified_at", nullable = false)
+    var modifiedAt: Instant,
+
+    @Column(name = "verified_at", nullable = true)
+    var verifiedAt: Instant,
+
+    @Column(name = "account_role", nullable = false)
+    var accountRole: AccountRole
+)
