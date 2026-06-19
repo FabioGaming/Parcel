c2b3a57f9614542088c033cd247bb01d0220b901
Fabio Gaming <fabio.furch@gmail.com>
2026-06-19 11:41:53 +0200

    Added basic logging to EventTypeScanner.kt

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/shared/events/EventTypeScanner.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/shared/events/EventTypeScanner.kt
@@ -2,6 +2,7 @@ package net.fabidev.parcelserver.shared.events
 
 import net.fabidev.parcelserver.shared.events.abstractions.DomainEvent
 import net.fabidev.parcelserver.shared.events.implementation.redis.DefaultEventTypeResolver
+import org.slf4j.LoggerFactory
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
@@ -11,6 +12,8 @@ class EventTypeScanner(
     private val resolver: DefaultEventTypeResolver
 ) : ApplicationRunner {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun run(args: ApplicationArguments) {
 
         val reflections = org.reflections.Reflections("net.fabidev.parcelserver")
@@ -19,6 +22,10 @@ class EventTypeScanner(
 
         eventTypes.forEach { clazz ->
             resolver.register(clazz as Class<out DomainEvent>)
+
+            logger.info("Registered EventType: ${clazz.simpleName}")
         }
+
+        logger.info("Registered ${eventTypes.count()} event types")
     }
 }
