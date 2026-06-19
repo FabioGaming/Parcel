efb424e20092e0306ec133b6f7416c60ddda0549
Fabio Gaming <fabio.furch@gmail.com>
2026-06-19 11:17:02 +0200

    Updated EventConsumer.kt to have a supports function that checks if its compatible with a DomainEvent

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/shared/events/abstractions/EventConsumer.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/shared/events/abstractions/EventConsumer.kt
@@ -1,5 +1,8 @@
 package net.fabidev.parcelserver.shared.events.abstractions
 
 interface EventConsumer<T : DomainEvent> {
+
+    fun supports(event: DomainEvent): Boolean
+
     fun handle(event: T)
 }
