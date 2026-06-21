81ec052d1ab20f0714fa50ca2f5ac116d7e4dacb
Fabio Gaming <fabio.furch@gmail.com>
2026-06-21 04:08:54 +0200

    Added basic REST Mapping to RegisterController.kt

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/routes/register/RegisterController.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/routes/register/RegisterController.kt
@@ -1,9 +1,18 @@
 package net.fabidev.parcelserver.modules.auth.routes.register
 
+import jakarta.validation.Valid
+import net.fabidev.parcelserver.modules.auth.routes.register.dtos.RegisterRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/auth/register")
 class RegisterController(val registerUseCase: RegisterUseCase) {
+
+    @PostMapping
+    fun register(@Valid @RequestBody body: RegisterRequest) {
+        
+    }
 }
