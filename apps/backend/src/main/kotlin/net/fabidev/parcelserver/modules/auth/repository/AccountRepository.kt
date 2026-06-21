88af3c5f9e87aef338e6a710d1d3feaaa8a345f2
Fabio Gaming <fabio.furch@gmail.com>
2026-06-21 04:09:16 +0200

    Modified AccountRepository.kt to include checking if an account exists by email

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/repository/AccountRepository.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/repository/AccountRepository.kt
@@ -4,4 +4,6 @@ import net.fabidev.parcelserver.modules.auth.entity.Account
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface AccountRepository : JpaRepository<Account, UUID> {}
+interface AccountRepository : JpaRepository<Account, UUID> {
+    fun existsAccountByEmail(email: String): Boolean
+}
