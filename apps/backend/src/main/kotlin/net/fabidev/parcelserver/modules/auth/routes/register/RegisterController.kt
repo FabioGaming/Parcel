5e62103d01764fbbe67c09cdbfd3e3dc378a03f8
Fabio Gaming <fabio.furch@gmail.com>
2026-06-22 08:52:25 +0200

    RegisterController.kt now gets device id request header for the register request

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/routes/register/RegisterController.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/routes/register/RegisterController.kt
@@ -2,8 +2,10 @@ package net.fabidev.parcelserver.modules.auth.routes.register
 
 import jakarta.validation.Valid
 import net.fabidev.parcelserver.modules.auth.routes.register.dtos.RegisterRequest
+import net.fabidev.parcelserver.shared.Constants
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 class RegisterController(val registerUseCase: RegisterUseCase) {
 
     @PostMapping
-    fun register(@Valid @RequestBody body: RegisterRequest) {
+    fun register(@Valid @RequestBody body: RegisterRequest, @RequestHeader(Constants.HEADER_DEVICE_ID) deviceId: String) {
+
     }
 }
