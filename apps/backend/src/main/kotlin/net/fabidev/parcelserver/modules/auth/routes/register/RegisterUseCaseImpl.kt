751db3895d9595457bc90a11601eafa99c6eca0f
Fabio Gaming <fabio.furch@gmail.com>
2026-06-22 08:51:59 +0200

    Adjusted RegisterUseCaseImpl.kt to use new interface contract

--- apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/routes/register/RegisterUseCaseImpl.kt
+++ apps/backend/src/main/kotlin/net/fabidev/parcelserver/modules/auth/routes/register/RegisterUseCaseImpl.kt
@@ -2,6 +2,7 @@ package net.fabidev.parcelserver.modules.auth.routes.register
 
 import net.fabidev.parcelserver.modules.auth.entity.Account
 import net.fabidev.parcelserver.modules.auth.repository.AccountRepository
+import net.fabidev.parcelserver.modules.auth.routes.register.dtos.RegisterRequest
 import net.fabidev.parcelserver.modules.auth.shared.PasswordHasher
 import net.fabidev.parcelserver.shared.events.abstractions.DomainEventPublisher
 import org.springframework.stereotype.Service
@@ -9,10 +10,9 @@ import org.springframework.stereotype.Service
 @Service
 class RegisterUseCaseImpl(private val passwordHasher: PasswordHasher, private val accountRepository: AccountRepository, private val eventPublisher: DomainEventPublisher) : RegisterUseCase {
 
-    override fun execute(email: String, password: String, deviceId: String): RegisterResult {
-        if(this.accountRepository.existsAccountByEmail(email)) {
+    override fun execute(request: RegisterRequest, deviceId: String): RegisterResult {
+        if(this.accountRepository.existsAccountByEmail(request.email)) {
             return RegisterResult.Error.EmailAlreadyUsed;
         }
-
     }
 }
